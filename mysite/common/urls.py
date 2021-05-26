298393edfd56be24515f1cb866db0e5f4e52e8af
joielee <2j00923@gmail.com>
2021-05-26 21:16:38 +0900

    chapter 03-6

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'common'
 
 urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'), #view를 참조할 때는 common의 login.html을 참조하라
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+  path('signup/', views.signup, name='signup'),
 ]
