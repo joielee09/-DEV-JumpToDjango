80a664710796802f55c6c5709a160347829884a1
joielee <2j00923@gmail.com>
2021-05-26 21:26:16 +0900

    chapter 03-7

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question
 from django.utils import timezone
 from .forms import *
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def index(request): # index 함수의 매개변수 request는 장고에 의해 자동으로 전달되는 HTTP 요청 객체.
   
@@ -30,6 +31,7 @@ def detail(request, question_id):
   # return HttpResponse("답변이 아직 작성되지 않았습니다.")
   return render(request, 'pybo/question_detail.html', context)
 
+@login_required
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -39,6 +41,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # 추가한 속성 author 적용
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -48,6 +51,7 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required
 def question_create(request):
     """
     pybo 질문생성
@@ -56,6 +60,7 @@ def question_create(request):
       form = QuestionForm(request.POST) 
       if form.is_valid(): # POST 요청 받은 form이 유효한 지 검사
         question = form.save(commit=False) # commit=False: 임시저장
+        question.author = request.user  # 추가한 속성 author 적용
         question.create_date = timezone.now() # question 객체를 반환 받아 create_date 값 설정 후
         question.save() # 데이터로 실제 저장
         return redirect('pybo:index')
